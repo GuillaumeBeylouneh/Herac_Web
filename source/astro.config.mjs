067833c4e534617a5160e8c679a23323450c61d8
GuillaumeBeylouneh <contact@ingenieur-developpeur.com>
2026-02-21 19:17:54 +0100

    feat: add sitemap.xml and robots.txt for SEO indexing

--- source/astro.config.mjs
+++ source/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  site: 'https://guillaumebeylouneh.github.io',
+  site: 'https://guillaumebeylouneh.github.io/Herac_Web',
   base: '/Herac_Web',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
